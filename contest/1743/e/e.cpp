61c6dc8a12f258ef63fa396c3e589ba439214520
cuzperf <cuzperf@outlook.com>
2022-10-18 02:26:53 +0800

    update 1743e

--- contest/1743/e/e.cpp
+++ contest/1743/e/e.cpp
@@ -3,26 +3,26 @@
 #define cerr(x) std::cerr << (#x) << " is " << (x) << '\n'
 using LL = long long;
 
-//
-// p1 > p2, t1 > t2
-LL f(int p1, int p2, LL t1, LL t2, int h) {
-  LL ans = INT64_MAX, cur = 0;
-  while (h > 0) {
-    ans = std::min(ans, cur + (h + p2 - 1) / p2 * t2);
-    cur += t1;
-    h -= p1;
-  }
-  ans = std::min(ans, cur);
-  return ans;
-}
-
 void solve() {
   int p1, p2, h, s;
   LL t1, t2;
   std::cin >> p1 >> t1 >> p2 >> t2 >> h >> s;
-  LL ans1 = f(p1 + p2 - s, p2 - s, std::max(t1, t2), t2, h);
-  LL ans2 = f(p1 + p2 - s, p1 - s, std::max(t1, t2), t1, h);
-  std::cout << std::min(ans1, ans2) << '\n';
+  p1 -= s; p2 -= s;
+  std::vector<LL> dp(h + 1);
+  for (int i = 1; i <= h; ++i) {
+    dp[i] = std::min(dp[std::max(0, i - p1)] + t1, dp[std::max(0, i - p2)] + t2);
+    for (int j = 1; j <= i; ++j) {
+      if (j * t1 >= t2) {
+        LL dmg = (j - 1) * p1 + (j * t1 / t2 - 1) * p2 + (p1 + p2 + s);
+        dp[i] = std::min(dp[i], dp[std::max(0LL, i - dmg)] + j * t1);
+      }
+      if (j * t2 >= t1) {
+        LL dmg = (j - 1) * p2 + (j * t2 / t1 - 1) + (p1 + p2 + s);
+        dp[i] = std::min(dp[i], dp[std::max(0LL, i - dmg)] + j * t2);
+      }
+    }
+  }
+  std::cout << dp[h] << '\n';
 }
 
 int main() {
